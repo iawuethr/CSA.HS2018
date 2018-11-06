b334ac8ab4b4fad0aed84faac1b072bbdf88c1aa
= <sandro.lampart@jmc-software.ch>
2018-11-06 19:05:34 +0100

    Testat dry implementation

---
+++ Testat/Form1.cs
@@ -0,0 +1,130 @@
+﻿using RobotCtrl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Testat
+{
+    public partial class Form1 : Form
+    {
+        private Robot _robot = new Robot();
+        private bool _isRunning = false;
+        private Thread _ledBlinkThread;
+        private Thread _counterThread;
+        private Thread _workerThread;
+        private readonly float _margin = 0.05f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radarView1.Radar = new Radar(Constants.IORadarSensor);
+            _robot.Drive.DriveCtrl.Power = true;
+
+            _robot.RobotConsole[Switches.Switch2].SwitchStateChanged += SwitchTwoChanged;
+        }
+
+        private void LetLedBlink()
+        {
+            _robot.RobotConsole[Leds.Led1].LedEnabled = !_robot.RobotConsole[Leds.Led1].LedEnabled & _isRunning;
+            _robot.RobotConsole[Leds.Led2].LedEnabled = !_robot.RobotConsole[Leds.Led1].LedEnabled & _isRunning;
+            _robot.RobotConsole[Leds.Led3].LedEnabled = !_robot.RobotConsole[Leds.Led1].LedEnabled & _isRunning;
+            _robot.RobotConsole[Leds.Led4].LedEnabled = !_robot.RobotConsole[Leds.Led1].LedEnabled & _isRunning;
+        }
+
+        private void SwitchTwoChanged(object sender, SwitchEventArgs e)
+        {
+            if (e.SwitchEnabled)
+            {
+                Start();
+            }
+            else
+            {
+                Stop();
+            }
+
+        }
+
+        private void Count()
+        {
+            // Let the LED blinking
+            _ledBlinkThread = new Thread(LetLedBlink);
+            _ledBlinkThread.Start();
+
+            float last = -(_margin + 0.01f);
+            int count = 0;
+            while (_isRunning)
+            {
+                float current = _robot.Radar.Distance;
+
+                if (current <= 1 && (last - current <= -_margin || last - current >= _margin))
+                {
+                    last = current;
+                    count++;
+                }
+            }
+        }
+
+        private void Start()
+        {
+            if (!_isRunning)
+            {
+                _isRunning = true;
+                _workerThread = new Thread(DoWork);
+                _workerThread.Start();
+            }
+        }
+
+        private void Stop()
+        {
+            _isRunning = false;
+
+            // So that the led blinker and counter are able to stop..
+            Thread.Sleep(50);
+
+            _ledBlinkThread.Abort();
+            _counterThread.Abort();
+        }
+
+        private void WaitDriveDone(int intervalWait)
+        {
+            while (!_robot.Drive.Done)
+            {
+                if (!_isRunning)
+                {
+                    Stop();
+                    return;
+                }
+
+                Thread.Sleep(intervalWait);
+            }
+        }
+
+        private void DoWork()
+        {
+            // Let the counter start counting
+            _counterThread = new Thread(Count);
+            _counterThread.Start();
+
+            _robot.Drive.RunLine(2.5f, 0.3f, 0.5f);
+
+            WaitDriveDone(500);
+
+            _robot.Drive.RunTurn(180f, 0.3f, 0.5f);
+
+            WaitDriveDone(500);
+
+            _robot.Drive.RunLine(2.5f, 0.3f, 0.5f);
+        }
+    }
+}
