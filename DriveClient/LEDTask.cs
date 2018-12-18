fd58126a5996185e0ec11664c61c1d1933bb6659
Markus Wüthrich <markus.wuethrich@stud.hslu.ch>
2018-12-18 15:01:45 +0100

    Testat 2 first part

---
+++ DriveClient/LEDTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RobotCtrl;
+
+namespace DriveClient
+{
+    public class LEDTask
+    {
+        private int LEDid = 0;
+
+        public void Run( Robot robot )
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                robot.RobotConsole[(Leds)LEDid].LedEnabled = false;
+
+                // Do sequence 0 to 4 over and over
+                LEDid++;
+                LEDid %= 4;
+
+                robot.RobotConsole[(Leds)LEDid].LedEnabled = true;
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ DriveClient/ScannerTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RobotCtrl;
+
+namespace DriveClient
+{
+    // Man I wish it wasnt 3.5
+    public class ScannerTask
+    {
+
+        public event EventHandler<EventArgs> scanAlert;
+
+        private float lastDist = float.MaxValue;
+        public void Run(Robot robot)
+        {
+            float currentDist;
+            while (Thread.CurrentThread.IsAlive)
+            {
+                currentDist = robot.Radar.Distance;
+                if (currentDist < 0.8f && lastDist >= 0.8f)
+                {
+                    scanAlert?.Invoke(this, new EventArgs());
+                }
+                lastDist = currentDist;
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
